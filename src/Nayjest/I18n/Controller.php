f90ca47c5917461d3486c7dbb3d49416f50eee95
github-4-work <n.mikhalchuk@lucky-labs.com>
2019-10-25 17:31:11 +0300

    Update Controller.php
    updates for subdomains supports

--- src/Nayjest/I18n/Controller.php
+++ src/Nayjest/I18n/Controller.php
@@ -7,20 +7,20 @@ use Cookie;
 use Lang;
 use Redirect;
 use Response;
+use Config;
 
 
 class Controller extends BaseController
 {
     public function switchLanguage($language)
     {
-        if (!I18n::getInstance()->isValidLanguage($language))
-        {
+        if (!I18n::getInstance()->isValidLanguage($language)) {
             return Response::json([
-                'status' => 'error',
+                'status'  => 'error',
                 'message' => Lang::trans('language_unsupported', e($language))
             ]);
         }
-        $cookie = Cookie::forever(I18n::SESSION_KEY, $language, '/');
+        $cookie = Cookie::forever(I18n::SESSION_KEY, $language, Config::get('session.path', null), Config::get('session.domain', null));
         return Redirect::back()->withCookie($cookie);
     }
 } 
